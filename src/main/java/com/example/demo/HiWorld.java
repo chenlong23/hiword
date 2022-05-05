77914ac5828e9ff368bb2232fed6b649eea26ade
陈龙 <1905516348@qq.com>
2022-05-05 14:31:16 +0800

    test

--- src/main/java/com/example/demo/HiWorld.java
+++ src/main/java/com/example/demo/HiWorld.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("HI World");
+        System.out.println("HI World  ");
     }
 }
